1ba61378f2258ccd4b80b22fd09cbb06876fcd28
TA-PP <kgrewd@naver.com>
2024-01-17 16:38:16 +0900

    Fix: modify network diagram

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from streamlit_option_menu import option_menu
 import main_page, network_diagram_page
 
 def create_layout():
+    # 사이드 바 로고
+    container = st.sidebar.container()
+    container.image('./images/logo.png', width=75)
     with st.sidebar:
         page = option_menu("Menu", ["Main", "User Inputs", "Network Diagram", 'Analysis'],
-                            icons=['house', 'person', 'bi bi-robot', 'bi bi-robot'],
+                            icons=['house', 'person', 'bar-chart', 'clipboard-data'],
                             menu_icon="app-indicator", default_index=0,
                             styles={
             "container": {"padding": "4!important", "background-color": "#fafafa"},
             "icon": {"color": "black", "font-size": "25px"},
             "nav-link": {"font-size": "16px", "text-align": "left", "margin":"10px", "--hover-color": "#fafafa"},
-            "nav-link-selected": {"background-color": "#08c7b4"},
+            "nav-link-selected": {"background-color": "#014f9e", "icon-color": "white"},
         }
         )
         
