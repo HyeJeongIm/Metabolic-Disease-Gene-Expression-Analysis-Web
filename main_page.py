1ca54ef73404fe9946d3f1b42a63fd57400df8d2
TA-PP <kgrewd@naver.com>
2024-01-18 04:27:07 +0900

    Feat: main image

--- main_page.py
+++ main_page.py
@@ -26,8 +26,11 @@ def create_data_statistics():
     st.subheader('Data statistics')
     st.image('images/test_image.jpg')
 
+def show_main_image():
+    st.image('./images/main_image.png')
     
 def write_main_page():
     create_header()
     create_search_bar()
+    show_main_image()
     create_data_statistics()
