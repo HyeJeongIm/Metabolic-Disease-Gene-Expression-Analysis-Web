0b3365ce489d5521c9e7fca675bd19abdd334bea
HyeJeongIm <dlaowjde@naver.com>
2024-01-23 21:13:14 +0900

    Feat: create gene list heatmap

--- gene_list_page.py
+++ gene_list_page.py
@@ -1,10 +1,29 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def create_header():
     st.title('Multiple Gene Expression Profile')
 
-def create_search_bar():
-    pass
-
+def show_heatmap(df, genes):
+    
+    if genes:
+        df_filtered = df.set_index('Gene name').loc[genes, genes]
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(df_filtered, annot=True, cmap='vlag')
+        st.pyplot(plt)
+    else:
+        st.error('Please enter valid gene names.')
+        
 def write_gene_list_page():
     create_header()
+    
+    genes_input = st.text_area('Enter gene names separated by commas:')
+    genes_list = [gene.strip() for gene in genes_input.split('\n') if gene]
+    
+    file_path = 'data\Gene-Gene Expression Correlation\Full Correlation Matrix\GeneGeneCorrelation_Adipose_LH_Sample.txt'
+    
+    df = pd.read_csv(file_path, sep='\t')
+    
+    show_heatmap(df, genes_list)
