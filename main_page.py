55fd763e3831e84de71a8251c44c6930c65fc89c
HyeJeongIm <dlaowjde@naver.com>
2024-01-23 18:31:24 +0900

    Feat: add y-axis to box plot

--- main_page.py
+++ main_page.py
@@ -14,7 +14,7 @@ def create_search_bar():
         
         # 검색이 수행된 후에만 수행
         if 'search_pressed' in st.session_state and st.session_state['search_pressed']:
-            one_gene_search.show_box_plot(st.session_state['gene_name'])
+            one_gene_search.show_box_plot(st.session_state['gene_name'], z_score=False)
             one_gene_search.show_network_diagram(st.session_state['gene_name'])
 
         if st.button('Back'):
