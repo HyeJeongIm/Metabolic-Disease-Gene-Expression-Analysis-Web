96598fbffa2f78dbbdd5ec73b8b1bb5a393f882f
TA-PP <kgrewd@naver.com>
2024-02-02 14:38:39 +0900

    Fix: express p-value threshold

--- gene_list_page.py
+++ gene_list_page.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import os
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
