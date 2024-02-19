36f85925b4f1951c6f890d167b680b9b86896588
HyeJeongIm <dlaowjde@naver.com>
2024-02-20 02:11:38 +0900

    Feat: modify gene list network with correlation

--- gene_list_page.py
+++ gene_list_page.py
@@ -121,42 +121,52 @@ def show_interaction(genes_list):
     with st.spinner('그래프를 그리는 중입니다...'):
         folder_path = './data/Gene-Gene Interaction/BIOGRID-ORGANISM-Homo_sapiens-4.4.229.tab3.txt'
         data = pd.read_csv(folder_path, sep='\t')
-
         df = pd.DataFrame(data)
 
-        # NetworkX 그래프 생성
-        net = Network(
-            notebook=True,
-            directed=False,
-        )
+        net = Network(notebook=True, directed=False)
+
+        # 그룹 선택 및 threshold 입력
+        sample_class = ['Adipose_LH', 'Adipose_OH', 'Adipose_OD', 'Liver_LH', 'Liver_OH', 'Liver_OD', 'Muscle_LH', 'Muscle_OH', 'Muscle_OD']
+        group = st.selectbox('Choose one group', sample_class, key='sample_input')
+        threshold = st.number_input('Enter threshold:', min_value=0.0, value=0.5, step=0.01)
+
+        # Correlation 데이터 로드
+        df_correlation_filtered = load_correlation_data(group, threshold)
 
         for gene_pair in combinations(genes_list, 2):
-            # gene_pair의 상호 작용을 데이터프레임에 추가
             interactions = df[((df['Official Symbol Interactor A'] == gene_pair[0]) & (df['Official Symbol Interactor B'] == gene_pair[1])) |
                             ((df['Official Symbol Interactor A'] == gene_pair[1]) & (df['Official Symbol Interactor B'] == gene_pair[0]))]
             if not interactions.empty:
-                # 노드 추가
-                net.add_node(gene_pair[0], label=gene_pair[0], title=gene_pair[0], size=15)
-                net.add_node(gene_pair[1], label=gene_pair[1], title=gene_pair[1], size=15)
-                # 엣지 추가
-                net.add_edge(gene_pair[0], gene_pair[1], color='skyblue')
+                net.add_node(gene_pair[0], label=gene_pair[0], title=gene_pair[0], size=15, color='grey')
+                net.add_node(gene_pair[1], label=gene_pair[1], title=gene_pair[1], size=15, color='grey')
 
-    # 네트워크 그리기
-    net.show_buttons(filter_=['physics'])
-    net.show("pyvis_net_graph.html")
+                correlation = df_correlation_filtered[(df_correlation_filtered['Gene'] == gene_pair[0]) & (df_correlation_filtered['Gene.1'] == gene_pair[1]) | (df_correlation_filtered['Gene'] == gene_pair[1]) & (df_correlation_filtered['Gene.1'] == gene_pair[0])]
+                if not correlation.empty:
+                    color = 'red' if correlation['Correlation coefficient'].values[0] > 0 else 'blue'
+                else:
+                    color = 'lightgrey'  
 
-    # html 파일 페이지에 나타내기
-    HtmlFile = open('pyvis_net_graph.html', 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    components.html(source_code, width=670, height=1070)
+                net.add_edge(gene_pair[0], gene_pair[1], color=color)
+
+        # 네트워크 그리기
+        net.show_buttons(filter_=['physics'])
+        net.show("pyvis_net_graph.html")
+
+        HtmlFile = open('pyvis_net_graph.html', 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        components.html(source_code, width=670, height=1070)
+    
+def load_correlation_data(group, threshold):
+    file_path = f'data/Gene-Gene Expression Correlation/Correlation Higher Than 0.5/GeneGene_HighCorrelation_{group}_0.5.txt'
+    df_correlation = pd.read_csv(file_path, sep='\t')
+    df_correlation_filtered = df_correlation[df_correlation['Correlation coefficient'].abs() >= threshold]
+    return df_correlation_filtered
 
 def write_gene_list_page():    
     create_header()
     
     genes_input = st.text_area('Enter gene names:')
-    # 여러 구분자 처리
     genes_list = re.split('[ ,\t\n]+', genes_input.strip())
-    
     
     st.write(f"Number of genes entered: {len(genes_list)}")  # Debug print
     base_path = 'data/Gene Expression/Z_Score'
